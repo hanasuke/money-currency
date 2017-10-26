acb7e867074b8d254efd8a97de5b02fee2be8117
naosuke <info@naosuke.me>
2017-10-27 01:52:29 +0900

    fix

--- test/money_test.rb
+++ test/money_test.rb
@@ -6,6 +6,6 @@ class MoneyTest < Test::Unit::TestCase
   def test_multiplication
     five = Dollar.new(5)
     five.times(2)
-    assert_equals 10, five.amount
+    assert_equal 10, five.amount
   end
 end
