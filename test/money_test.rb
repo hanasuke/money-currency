1d1dddb962b619d6d4b4c32daa13a6fccd3c977f
naosuke <info@naosuke.me>
2017-10-27 01:50:43 +0900

    fix typo

--- test/money_test.rb
+++ test/money_test.rb
@@ -4,7 +4,7 @@ require_relative '../money/dollar'
 
 class MoneyTest < Test::Unit::TestCase
   def test_multiplication
-    five = Doller.new(5)
+    five = Dollar.new(5)
     five.times(2)
     assert_equals 10, five.amount
   end
