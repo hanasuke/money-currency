496d1b8d4532914bd7ba8a61c19210f47d1b0ce3
naosuke <info@naosuke.me>
2017-10-31 01:17:39 +0900

    ==メソッドのオーバーライドを修正

--- money/franc.rb
+++ money/franc.rb
@@ -1,17 +1,11 @@
 require_relative './money'
 
 class Franc < Money
-  attr_reader :amount
-
   def initialize(amount)
     @amount = amount
   end
 
   def times(times)
     return Franc.new(amount * times)
-  end
-
-  def ==(obj)
-    @amount == obj.amount
   end
 end
