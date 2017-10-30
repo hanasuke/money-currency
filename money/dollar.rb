ecb01b4bc34190e8e62909fd9024737f2973097e
naosuke <info@naosuke.me>
2017-10-31 01:16:12 +0900

    Dollar#==をMoney#==にリファクタリング

--- money/dollar.rb
+++ money/dollar.rb
@@ -1,17 +1,11 @@
 require_relative './money'
 
 class Dollar < Money
-  attr_reader :amount
-
   def initialize(amount)
     @amount = amount
   end
 
   def times(times)
     return Dollar.new(amount * times)
-  end
-
-  def ==(obj)
-    @amount == obj.amount
   end
 end
--- money/money.rb
+++ money/money.rb
@@ -1,2 +1,7 @@
 class Money
+  attr_reader :amount
+
+  def ==(obj)
+    @amount == obj.amount
+  end
 end
