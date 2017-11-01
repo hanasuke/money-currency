d147358d659419d1e0dcd9f18df455f223361d9b
naosuke <info@naosuke.me>
2017-11-02 02:34:20 +0900

    Moneyにdollarクラスを定義し、サブクラスへの直接的な型参照を撲滅

--- money/money.rb
+++ money/money.rb
@@ -1,8 +1,11 @@
 class Money
   attr_reader :amount
 
-  def ==(obj)
-    @amount == obj.amount && self.class == obj.class
+  def self.dollar(amount)
+    return Dollar.new(amount)
+  end
 
+ def ==(obj)
+    @amount == obj.amount && self.class == obj.class
   end
 end
